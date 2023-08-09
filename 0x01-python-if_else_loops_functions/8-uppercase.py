02376563d12d2a782f37754ce934f26abfbee272
Constance Amatey <consamate57@gmail.com>
2023-08-08 17:23:07 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    new_string = " "
-    for i in str:
-        ord(i)
-        return(i)
-    print(str)
+    for Ascii in str:
+        if ord(Ascii) >= ord('a') and ord(Ascii) <= ord('z'):
+            Ascii = chr(ord(Ascii) - 32)
+        print("{:s}".format(Ascii), end="")
+    print()
