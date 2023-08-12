0f7ce84e0fbaea5f1944080f6224796e6bfb6fb1
Constance Amatey <consamate57@gmail.com>
2023-08-11 20:44:04 -0700

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 
-import sys
-if (len(sis.argv)) == 1:
-    print("argument")
-elif (len(sis.argv)) > 1:
-    print("arguments")
-else:
-    print(".")
+if __name__ == "__main__":
 
+    import sys
+
+    count = len(sys.argv) - 1
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{d:} arguments:".format(count))
+    for i in range(count):
+        print("{d:}: {}".format(i + 1, sys.argv[i + 1]))
