ad52e2446113bfc62c907436448c1b5b925a6c86
Constance Amatey <consamate57@gmail.com>
2023-08-29 11:10:42 -0700

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a class called Square"""
+
+
+class Square:
+
+    """ Defines a private attribute called size"""
+    def __init__(self, size):
+
+        """initializes the private attribute"""
+        self.__size = size
