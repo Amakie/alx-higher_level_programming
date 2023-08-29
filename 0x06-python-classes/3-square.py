3497ad44889646f6024ec13ba9955c4553761d5c
Constance Amatey <consamate57@gmail.com>
2023-08-29 14:46:03 -0700

    public attribute

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,16 +8,16 @@ class Square:
     """Initializes a private attribute called size"""
     def __init__(self, size=0):
 
+        """defines the private attribute"""
+        self.__size = size
+
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif (size < 0):
             raise ValueError("size must be >= 0")
 
-        """defines the private attribute"""
-        self.__size = size
+    """defines a public instance method"""
+    def area(self):
 
-        """defines a public instance method"""
-        def area(self):
-
-            """returns the current area"""
-            return (self.__size ** 2)
+        """returns the current area"""
+        return (self.__size ** 2)
