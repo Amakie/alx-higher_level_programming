7ac04ada6fed79ab29a8dc0670b8c1725049f5a1
Constance Amatey <consamate57@gmail.com>
2023-09-05 07:21:55 -0700

    locked class

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+"""Defines a locked class called LockedClass"""
+class LockedClass:
+    """
+    A class that does not allow any other instances
+    Unless it called first name
+    """
+    __slot__ = [first_name]
