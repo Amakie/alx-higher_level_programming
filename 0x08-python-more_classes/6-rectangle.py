8d50275c18b90d22dafb8782c3cbdfe28f1caf91
Constance Amatey <consamate57@gmail.com>
2023-09-04 19:51:44 -0700

    printing instamces

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -7,17 +7,17 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-         """Initializes this rectangle class
-         Arguments:
+        """Initializes this rectangle class
+        Arguments:
             width: represents the width of the rectangle
             height: represents the height of the rectangle
-         Raises:
+        Raises:
             TypeError: if size is not integer
             ValueError: if size is less than zero
         """
-         self.width = width
-         self.height = height
-         Rectangle.number_of_instances += 1
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
