da33aa08cfe3a3a221f1f2b8f36d3786f952a68a
Constance Amatey <consamate57@gmail.com>
2023-09-04 18:31:59 -0700

    rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -74,5 +74,6 @@ class Rectangle:
 
     def __del__(self):
         """prints a message for every object that is deleted"""
+        if type(self).number_of_instances > 0:
+            type(self).number_of_instances -= 1
         print("Bye rectangle...")
-        Rectangle.number_of_instances -= 1
