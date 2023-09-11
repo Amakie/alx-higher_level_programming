b6d28f094cc940134d570fe277aff803648ec953
Constance Amatey <consamate57@gmail.com>
2023-09-11 16:09:36 -0700

    same class task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""defines an object using booleen values"""
+
+
+def is_same_class(obj, a_class):
+    """This functions defines the object"""
+
+    if (type(obj) == a_class):
+        return True
+    else:
+        return False
