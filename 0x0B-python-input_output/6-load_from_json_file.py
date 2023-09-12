6ace61dc2444934cd1babe4c1a1b8935e8a9c58f
Constance Amatey <consamate57@gmail.com>
2023-09-12 15:51:21 -0700

    create python file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creates an object froma json file"""
+
+import json
+
+
+def load_from_json_file(filename):
+    """obj represntation of a json file"""
+    with open(filename) as f:
+        return (json.loads(f))
