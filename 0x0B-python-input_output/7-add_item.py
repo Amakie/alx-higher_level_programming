c08cb865acfd27960c4928475d1c9c02335c939e
Constance Amatey <consamate57@gmail.com>
2023-09-12 16:17:26 -0700

    adds items to a python list

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Function that adds all arguments to a Python list"""
+""" a script that adds all arguments to a
+Python list, and then save them to a file
+"""
 
 
 import sys
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
+def add_items(args):
     try:
-        items = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        items = []
-    items.extend(sys.argv[1:])
-    save_to_json_file(items, "add_item.json")
+        py_list = load_from_json_file("add_item.json")
+    except Exception:
+        py_list = []
+    py_list += args
+    save_to_json_file(py_list, "add_item.json")
+
+    if __name__ == "__main__":
+        add_item(sys.argv[1:])
