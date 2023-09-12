e11cae59066d7dfcee9881ace4780823ddc37d93
Constance Amatey <consamate57@gmail.com>
2023-09-11 17:07:53 -0700

    raising exceptions

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module that defines a class"""
+
+
+class BaseGeometry:
+    """created a class called BaseGeometry"""
+
+    def area(self):
+        """raises an exeption"""
+
+        raise Exception("area() is not implemented")
