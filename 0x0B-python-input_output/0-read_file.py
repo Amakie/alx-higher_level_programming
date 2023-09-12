848467bc574be8f25a771decdb7c85a074736ff4
Constance Amatey <consamate57@gmail.com>
2023-09-12 13:41:21 -0700

    read and print file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,5 @@ def read_file(filename=""):
     reads from the file
     """
 
-    with open(filename, r, encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
