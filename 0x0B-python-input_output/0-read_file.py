d2a12abbd7dfed656618f1a119149a1eafb0ea60
Constance Amatey <consamate57@gmail.com>
2023-09-12 12:53:52 -0700

    read and print file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    """function opens a file
+    reads from the file
+    """
+
+    with open(filename, r, encoding="UTF8") as f:
+        print(f.read(), end="")
