2fd0e5a61ce000b1a9146080f67572e8efc47f62
Constance Amatey <consamate57@gmail.com>
2023-09-16 17:08:37 -0700

    test cases

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""This module creates a base class"""
+
+
+class Base:
+    """This creates a class called Base"""
+    pass
+
---
+++ 0x0C-python-almost_a_circle/tests/test_base.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""test cases for project"""
+
+
+import unittest
+from models.base import Base
+from models.square import Square
+import json
+import inspect
+
+class test_base(unittest.Testcase):
+    """class for test cases"""
+
+    def test_no_id(self):
+        """no id"""
+        b = Base()
+        self.AssertEqual(1, b.id)
+
+     def test_valid_id(self):
+        """valid id"""
+        b = Base()
+        self.AssertEqual(50, b.id)
+
+    def test_zero_id(self):
+        """zero id"""
+        b = Base()
+        self.AssertEqual(0, b.id)
+
