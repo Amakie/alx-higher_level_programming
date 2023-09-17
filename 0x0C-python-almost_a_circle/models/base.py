32bf04761689de7d70c98aa201b2aed30721dcda
Constance Amatey <consamate57@gmail.com>
2023-09-17 09:55:56 -0700

    task 1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 """This module creates a base class"""
 
+import json
+import csv
+import turtle 
+import os
 
 class Base:
     """This creates a class called Base"""
-    pass
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """initializes attributes"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    
 
