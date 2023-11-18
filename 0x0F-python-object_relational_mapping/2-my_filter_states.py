c31db3dc49b0763284c429e10a379bb5b3b7408c
Constance Amatey <consamate57@gmail.com>
2023-11-18 20:35:10 +0000

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""scripts that lists all states"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    h = 'localhost'
+
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM `states` WHERE \
+                   name LIKE BINARY '{:s}' ORDER BY id ASC".format(argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close
+    db.close
