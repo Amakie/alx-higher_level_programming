e55c0396fa777dfecf52f5978af1e2ec13a1e88b
Constance Amatey <consamate57@gmail.com>
2023-11-19 20:01:35 +0000

    task 5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""scripts that lists all states"""
+
+from sys import argv
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+
+    u_name = sys.argv[1]
+    p = sys.argv[2]
+    db_name = sys.argv[3]
+    h = 'localhost'
+
+    db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
+    cursor = db.cursor()
+    cursor.execute("SELECT cities.name FROM cities \
+    INNER JOIN states ON states.id = cities.state_id \
+    WHERE states.name LIKE BINARY %s ORDER BY cities.id ASC", (argv[4],))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close
+    db.close
