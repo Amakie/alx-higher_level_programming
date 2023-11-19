15f622db094f9d8a1f462571ed8805925c79f332
Constance Amatey <consamate57@gmail.com>
2023-11-19 19:51:36 +0000

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(host=h, port=3306, user=u_name, passwd=p, db=db_name)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM `states` WHERE name \
-                   LIKE BINARY %s ORDER BY id ASC", (argv[4]))
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
+    ORDER BY id ASC", (argv[4],))
     rows = cursor.fetchall()
     for row in rows:
         print(row)
