eaa9f491c0f9dfaef105b16fe5db832d41e7a165
Constance Amatey <consamate57@gmail.com>
2023-11-20 01:58:22 +0000

    descriptive base

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""links class to database"""
+
+from model_state import Base, State
+import sys
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
