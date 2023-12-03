75bf25c00db677de9da2a37d2f7f8d835f51ce0f
Constance Amatey <consamate57@gmail.com>
2023-12-03 21:26:44 +0000

    debugged script

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Bash script to sent a POST request to passed URL and displays body
-curl -s "$1" -d "test@gmail.com" -d "subject=I will always be here for PLD"
+curl -s "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
