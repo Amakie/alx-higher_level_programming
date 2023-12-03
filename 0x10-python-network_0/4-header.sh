6da5a4399270b67e13a49f1dbd17bf631cd86759
Constance Amatey <consamate57@gmail.com>
2023-12-03 21:22:39 +0000

    debug

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
-curl -sI "$1" | grep "Allow" | cut -d ":" -f2 | cut -d " " --fields=2-
+curl -s -H "X-School-User-Id: 98" "$1"
