15d9f7a899cb7a8f92851f40163caf101a929f11
Constance Amatey <consamate57@gmail.com>
2024-01-03 18:21:44 +0000

    script that reads and prints the content of a file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+// script that reads and prints the content of a file
+
+const fs = require('fs');
+let passedFile = process.argv[2];
+
+fs.readFile(passedFile, 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
