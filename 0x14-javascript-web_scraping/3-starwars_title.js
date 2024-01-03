61ce6f7464ebc979d9d99530ce149fcc6301f02c
Constance Amatey <consamate57@gmail.com>
2024-01-03 19:09:19 +0000

    script that prints the title of a Star Wars movie

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,13 +1,16 @@
 #!/usr/bin/node
-// script that prints the title of a Star Wars movie where the episode number matches a given integer
-const request = require('request');
-const episodeNumber = process.argv[2];
-const requestURL = 'https://swapi-api.alx-tools.com/api/films/:id' + episodeNumber;
 
-request(requestURL, function (error, response, body) {
-  const theBody = JSON.parse(body);
-  console.log(theBody.title);
-  if (error) {
-    console.log(error);
+const request = require('request');
+const episodeNum = process.argv[2];
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/:id';
+
+request(API_URL + episodeNum, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    const responseJSON = JSON.parse(body);
+    console.log(responseJSON.title);
+  } else {
+    console.log('Error code: ' + response.statusCode);
   }
 });
