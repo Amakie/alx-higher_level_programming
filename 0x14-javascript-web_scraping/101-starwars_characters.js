c4a0c0c5a2150ca070690fbd8a435dfc3bc6496a
Constance Amatey <consamate57@gmail.com>
2024-01-03 19:34:58 +0000

    script that prints all characters of a Star Wars movie

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,37 @@
+#!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    const characters = movie.characters;
+
+    const characterNames = [];
+    const requests = characters.map((characterUrl) => {
+      return new Promise((resolve) => {
+        request(characterUrl, (charError, charResponse, charBody) => {
+          if (charError) {
+            console.error(charError);
+          } else {
+            const character = JSON.parse(charBody);
+            characterNames.push(character.name);
+            resolve();
+          }
+        });
+      });
+    });
+
+    Promise.all(requests).then(() => {
+      characterNames.forEach((name) => {
+        console.log(name);
+      });
+    });
+  }
+});
